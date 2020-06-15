ab89ef998fc124768d2ab7fb63dad2b5230fc629
Joel Antonio Lopez Javier <joel_Lopez1@ucne.edu.do>
2020-06-15 19:58:36 -0400

    En Proceso

---
+++ rArticulos/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RegistroArticulos.Entidades;
+
+namespace RegistroArticulos.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Articulos> Articulos { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuiler)
+        {
+            optionBuiler.UseSqlite(@"Data Source= DATA\BaseDeDatos.db");
+        }
+    }
+}
---
+++ rArticulos/Entidades/Articulos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroArticulos.Entidades
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+        public int Descripcion { get; set; }
+        public float Costo { get; set; }
+        public float Valor { get; set; }
+    }
+}
